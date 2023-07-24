10d60a14a7ae84076c250f83c3fd7ed56843ef75
DESKTOP-OP6CCKQ\HAVOK <alireza.karampoor@gmail.com>
2023-07-24 16:06:47 +0330

    fixed showcase layout

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
 	import Navbar from '$lib/Navbar.svelte';
+	import chat from '$lib/images/chat.png';
 </script>
 
 <Navbar />
+
+<div class="layout">
+	<div class="column">
+		<img src={chat} alt="chat" />
+	</div>
+	<div class="column">
+		<img src={chat} alt="chat" />
+	</div>
+</div>
+
+<style>
+	.layout {
+		display: flex;
+		flex-direction: row;
+	}
+
+	img {
+		max-width: 80%;
+		object-fit: cover;
+		border-radius: 15px;
+	}
+
+	.column {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
